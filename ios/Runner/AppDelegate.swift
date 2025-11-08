07e03d557f36e14c3072cab976734dfadc07d8f2
alhitii <mostafa.alheeti25@gmail.com>
2025-11-08 21:08:55 +0300

    Fix iOS crash by configuring Firebase before plugin registration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,14 +2,18 @@ import UIKit
 import Flutter
 import FirebaseCore
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // ✅ Firebase أولاً
     FirebaseApp.configure()
+
+    // ✅ بعدها تسجيل الإضافات
     GeneratedPluginRegistrant.register(with: self)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
